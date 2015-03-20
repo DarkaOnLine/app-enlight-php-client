a6a281cbe1b52ee337d7f8924415dca162b026ca
Darius Matulionis <darius@matulionis.lt>
2015-03-20 08:19:03 +0200

    New lines

--- libs/Framework/Laravel/src/ErrorHandler.php
+++ libs/Framework/Laravel/src/ErrorHandler.php
@@ -91,5 +91,4 @@ class ErrorHandler {
             unset($aeTrace);
         }
     }
-
 }
--- libs/Framework/Laravel/src/ServiceProvider.php
+++ libs/Framework/Laravel/src/ServiceProvider.php
@@ -66,6 +66,4 @@ class ServiceProvider  extends Provider
             'AppEnlight\Laravel\ErrorHandler'
         ];
     }
-
-
 }
